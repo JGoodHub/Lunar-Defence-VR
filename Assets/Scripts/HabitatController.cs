584208d90cb532ec06a2776a0d31c37388431d75
JGoodHub <jagoodmail@gmail.com>
2019-08-01 21:45:44 +0100

    Added a space based skybox and habitat domes, realism greatly increased

---
+++ Assets/Scripts/HabitatController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HabitatController : MonoBehaviour {
+
+    //VARIABLES
+
+	public int startingHealth;
+	private int currentHealth;
+
+    //METHODS
+    
+	public void InitialiseController () {
+		currentHealth = startingHealth;
+	}
+    
+}
