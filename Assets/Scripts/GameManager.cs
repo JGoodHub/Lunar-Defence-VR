d2b12ae0b01ca397cd0b863a5b71f9a93e5105c5
JGoodHub <jagoodmail@gmail.com>
2019-08-01 13:43:13 +0100

    Imported the google vr sdk

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    //SINGLETON PATTERN
+
+    public static GameManager instance = null;
+	
+	void Awake () {
+		if (instance == null) {
+			instance = this;
+		} else {
+			Destroy(gameObject);
+		}
+	}
+
+    //VARIABLES
+
+    //METHODS
+    
+    
+}
